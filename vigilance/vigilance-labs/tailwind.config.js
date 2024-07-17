a5026d2ba8ef80b026d599a881417692a2265fd9
Mezzky <ryansahadha@gmail.com>
2024-07-17 21:13:33 +0800

    Add: New version

---
+++ vigilance/vigilance-labs/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    screens: {
+      'sm': '320px',
+      'md': '375px', 
+      'lg': '425px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins',
+        arabic: 'IBM Plex Sans Arabic'
+      },
+      colors: {
+        box: 'rgba(120, 120, 120, 0.2)',
+        text: '#FBFBFB',
+        span: '#4BA7D8',
+        main: '#500DE0',
+        boxborder: '#4D4D4D',
+        bg: '#0C0F16',
+      },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(225, 225, 225, 0.25)'
+      },
+      backgroundImage: {
+        'home': "url('../Assets/gif/home-img.gif')",
+        'service': "url('../Assets/gif/service-bg.gif')",
+        'wavy': "url('../Assets/gif/wavy-bg-50.gif')",
+        'ombaks': "url('../Assets/gif/ombaks-bg.gif')",
+        'bumi': "url('../Assets/gif/blubumi-bg.gif')",
+        'hole': "url('../Assets/gif/hole-bg.gif')",
+        'recognition': "url('../Assets/gif/bumi-mutars.gif')"
+      }
+    },
+  },
+  plugins: [],
+}
+
